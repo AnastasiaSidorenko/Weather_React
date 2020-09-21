b8512b750c4f4e2e1570b5c8346efe433923dc63
Anastasia <39671563+AnastasiaSidorenko@users.noreply.github.com>
2020-09-21 18:28:13 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function AppRouter() {
+   return (
+      
+   )
+}
+
+export default AppRouter;
