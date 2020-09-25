9c27d22124e20cfd0ead6d4573c92ce0bd4286bf
Anastasia <39671563+AnastasiaSidorenko@users.noreply.github.com>
2020-09-25 12:31:45 +0300

    Updates

---
+++ precache-manifest.6314317d99b4753a8b4f4fe78f35b1e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72a77d13ad7f0ba276334a5cbd94781",
+    "url": "/Weather_React/index.html"
+  },
+  {
+    "revision": "9b5f595eb3a78e0808aa",
+    "url": "/Weather_React/static/css/main.5c5a8d54.chunk.css"
+  },
+  {
+    "revision": "0fdd2c3b82003fa064f9",
+    "url": "/Weather_React/static/js/2.b9f7384f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather_React/static/js/2.b9f7384f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b5f595eb3a78e0808aa",
+    "url": "/Weather_React/static/js/main.4b75abf0.chunk.js"
+  },
+  {
+    "revision": "8df3ccc75148a7fe6d7a",
+    "url": "/Weather_React/static/js/runtime-main.63e99b46.js"
+  }
+]);
